84883f645a3c1bbd4583ca04f16de85263659693
Eduardo Ochs <eduardoochs@gmail.com>
2019-04-08 02:47:10 -0300

    Added the files "eev.el" and ".gitignore".

---
+++ eev.el
@@ -0,0 +1,33 @@
+;;; eev.el --- Support for e-scripts (eepitch blocks, elisp hyperlinks, etc)
+
+;; Copyright (C) 2012-2019 Free Software Foundation, Inc.
+
+;; Author: Eduardo Ochs <eduardoochs@gmail.com>
+;; Package-Requires: ((emacs "24.3"))
+;; Keywords: lisp e-scripts
+;; URL: http://angg.twu.net/#eev
+;; Version: 20190401
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'eev)
+;;; eev.el ends here
