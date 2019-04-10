5f60818febd022ef1bfe9e9cd68f0e512320d142
Eduardo Ochs <eduardoochs@gmail.com>
2019-04-10 00:55:28 -0300

    Changed the copyrights from "(not yet?) part of GNU eev" to "part of GNU eev".

--- eev-anchors.el
+++ eev-anchors.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2012-2019 Free Software Foundation, Inc.
 ;;
-;; This file is (not yet?) part of GNU eev.
+;; This file is part of GNU eev.
 ;;
 ;; GNU eev is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
