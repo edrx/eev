d37c77f28d928044d60a694db4d49b548e3daf4a
Eduardo Ochs <eduardoochs@gmail.com>
2021-09-25 19:42:46 -0300

    Bumped the version in eev.el.

--- eev-intro.el
+++ eev-intro.el
@@ -19,7 +19,7 @@
 ;;
 ;; Author:     Eduardo Ochs <eduardoochs@gmail.com>
 ;; Maintainer: Eduardo Ochs <eduardoochs@gmail.com>
-;; Version:    20210817
+;; Version:    20210925
 ;; Keywords:   e-scripts
 ;;
 ;; Latest version: <http://angg.twu.net/eev-current/eev-intro.el>
--- eev.el
+++ eev.el
@@ -6,7 +6,7 @@
 ;; Package-Requires: ((emacs "24.4"))
 ;; Keywords: lisp e-scripts
 ;; URL: http://angg.twu.net/#eev
-;; Version: 20210913
+;; Version: 20210925
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
