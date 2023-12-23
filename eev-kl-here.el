210347643a61dd1a79eec8149b66b2429cdafd08
Eduardo Ochs <eduardoochs@gmail.com>
2023-12-22 23:47:02 -0300

    Added "eev-kl-here.el".

---
+++ eev-kl-here.el
@@ -0,0 +1,37 @@
+;;; eev-kl-here.el -- Kill link to here.  -*- lexical-binding: nil; -*-
+
+;; Copyright (C) 2023 Free Software Foundation, Inc.
+;;
+;; This file is part of GNU eev.
+;;
+;; GNU eev is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; GNU eev is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;; Author:     Eduardo Ochs <eduardoochs@gmail.com>
+;; Maintainer: Eduardo Ochs <eduardoochs@gmail.com>
+;; Version:    20231222
+;; Keywords:   e-scripts
+;;
+;; Latest version: <http://anggtwu.net/eev-current/eev-kl-here.el>
+;;       htmlized: <http://anggtwu.net/eev-current/eev-kl-here.el.html>
+;;       See also: <http://anggtwu.net/eev-current/eev-beginner.el.html>
+;;                 <http://anggtwu.net/eev-intros/find-kla-intro.html>
+;;                                               (find-kla-intro)
+
+
+
+
+;; Local Variables:
+;; coding:            utf-8-unix
+;; no-byte-compile:   t
+;; End:
