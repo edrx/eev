0365f9a21b8494a2fab75838022141b364731e25
Eduardo Ochs <eduardoochs@gmail.com>
2025-06-07 01:11:08 -0300

    Bumped the version in eev.el (for ELPA).

--- eev.el
+++ eev.el
@@ -1,12 +1,12 @@
 ;;; eev.el --- Support for e-scripts (eepitch blocks, elisp hyperlinks, etc)  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019-2024 Free Software Foundation, Inc.
+;; Copyright (C) 2019-2025 Free Software Foundation, Inc.
 
 ;; Author: Eduardo Ochs <eduardoochs@gmail.com>
 ;; Package-Requires: ((emacs "25.1"))
 ;; Keywords: lisp e-scripts
 ;; URL: http://anggtwu.net/#eev
-;; Version: 20250216
+;; Version: 20250607
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
